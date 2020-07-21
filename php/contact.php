bdf29bc8933b4c211e7077e5ef760809afa76d27
ryosuke.fukui <fukuiryo.0312@gmail.com>
2020-07-21 20:50:48 +0900

    modify layout, php file

--- php/contact.php
+++ php/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 $subject = "You have been contacted by ";
-$success_message = "Your messege has been sent sucesfully!";
-$fail_message = "Sorry, something went wrong! Please try again.";
+$success_message = "メッセージが送信されました";
+$fail_message = "メッセージの送信に失敗しました。時間を置いて再度お試しください。";
 
 // Configuration option.
 // Enter the email address that you want to emails to be sent to.
@@ -10,11 +10,12 @@ $fail_message = "Sorry, something went wrong! Please try again.";
 
 //$admin_email = "example@example.com";
 
-$admin_email = 'example@example.com'; //Replace this with your email id
+$admin_email = 'fukuiryo.0312@gmail.com'; //Replace this with your email id
 
 $validate = true;
 $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+$company = filter_var($_POST['company'], FILTER_SANITIZE_SPECIAL_CHARS);
 $msg = filter_var($_POST['message'], FILTER_SANITIZE_SPECIAL_CHARS);
 
 if (!($name && $email && $msg)) {
@@ -25,9 +26,10 @@ if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 
 // Message
 
-$e_body = "You have been contacted by $name. Their additional message is as follows." . PHP_EOL . PHP_EOL;
+$e_body = "$name 様からメッセージが送信されました。" . PHP_EOL . PHP_EOL;
 $e_content = "\"$msg\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $name via email, $email";
+$e_reply = "メールアドレス： $email";
+$e_company = "会社名： $company";
 
 
 $message = wordwrap( $e_body . $e_content . $e_reply, 70 );
@@ -39,7 +41,7 @@ $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
 // Mail it
-if ($validate && mail( $admin_email, "$subject by $name", $message, $headers)) {
+if ($validate && mail( $admin_email, "【GAS_STUDIO】 $name 様からのメッセージです", $message, $headers)) {
 	echo $success_message;
 } else {
 	echo $fail_message;
